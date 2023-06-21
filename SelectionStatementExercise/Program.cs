002334dae3add2768ae605bff506040b1bd4bd1c
Janine Masunzu <jamasunzu99@gmail.com>
2023-06-21 14:02:28 -0500

    finished the 1st exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // ----------Exercise 1
+            var r = new Random();
+            var favNumber = r.Next(1, 50);
+        
+         
+            Console.WriteLine("Pick a number from 1 - 50");
+            var userInput = int.Parse(Console.ReadLine());
+            
+            if (userInput < favNumber) 
+            { 
+                Console.WriteLine($"{userInput} is too low");
+
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high");
+
+            }
+            else 
+            {
+             Console.WriteLine("You got it right");
+            }
+
         }
     }
 }
